3d342c432901c832c3901673d2db8df15469c1d7
James McIntyre <james.checks.his.email@gmail.com>
2016-05-25 23:48:55 -0700

    scaffold

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+require("../styles/main.scss");
+
+// import HelloForm from './HelloForm';
+
+
+ReactDOM.render(
+    <div>Hello!</div>
+    , document.getElementById('view')
+);
